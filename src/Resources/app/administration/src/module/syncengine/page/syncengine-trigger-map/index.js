dd91752e2d496b41786a38b2d049c74543a161e9
JoryHogeveen <info@keraweb.nl>
2026-04-15 01:00:43 +0200

    Improve triggermap UI

--- src/Resources/app/administration/src/module/syncengine/page/syncengine-trigger-map/index.js
+++ src/Resources/app/administration/src/module/syncengine/page/syncengine-trigger-map/index.js
@@ -1,4 +1,5 @@
 import template from './syncengine-trigger-map.html.twig';
+import './syncengine-trigger-map.scss';
 
 const { Component, Filter } = Shopware;
 
@@ -26,6 +27,28 @@ Component.register('syncengine-trigger-map-page', {
             const date = new Date(this.updatedAt * 1000);
             return Filter.getByName('date')(date);
         },
+
+        columns() {
+            return [
+                {
+                    property: 'trigger',
+                    label: 'Trigger',
+                    primary: true,
+                    allowResize: true,
+                },
+                {
+                    property: 'endpointCount',
+                    label: 'Endpoint count',
+                    allowResize: true,
+                    align: 'right',
+                },
+                {
+                    property: 'endpointList',
+                    label: 'Endpoints',
+                    allowResize: true,
+                },
+            ];
+        },
     },
 
     created() {
@@ -45,6 +68,8 @@ Component.register('syncengine-trigger-map-page', {
                 this.rows = Object.entries(map).map(([trigger, endpoints]) => ({
                     trigger,
                     endpoints: Array.isArray(endpoints) ? endpoints : [],
+                    endpointCount: Array.isArray(endpoints) ? endpoints.length : 0,
+                    endpointList: Array.isArray(endpoints) ? endpoints.join(', ') : '',
                 }));
             } catch (e) {
                 this.error = e?.message || 'Failed to load trigger map';
